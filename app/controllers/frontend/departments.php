ab297f4b0e14d524358776f74ae29503126f609c
Alexander <92639298+Lykanthropie1@users.noreply.github.com>
2021-12-29 22:13:56 +0400

    fix controllers for showcase and dev templates for showcase

--- app/controllers/frontend/departments.php
+++ app/controllers/frontend/departments.php
@@ -27,7 +27,7 @@ if ($mode === 'view') {
             $departments[$department]['director_id'] = fn_get_user_short_info($departments[$department]['director_id']);
         } else {
             $departments[$department]['director_id'] = '';
-        };
+        }
         $departments = $departments;
     }
 
@@ -37,6 +37,7 @@ if ($mode === 'view') {
 
 
     fn_add_breadcrumb("Отделы");
+
 } elseif ($mode === 'info') {
     $department_data = [];
     $department_id = !empty($_REQUEST['department_id']) ? $_REQUEST['department_id'] : 0;
@@ -47,37 +48,14 @@ if ($mode === 'view') {
 
     Tygh::$app['view']->assign('department_data', $department_data);
 
-    fn_add_breadcrumb($department_data['collection']);
+    fn_add_breadcrumb($department_data['department']);
 
-    $params = $_REQUEST;
-    $params['extend'] = ['description'];
-    $params['item_ids'] = !empty($department_data['workers_ids']) ? implode(',', $department_data['workers_ids']) : -1;
-
-    if ($items_per_page = fn_change_session_param(Tygh::$app['session']['search_params'], $_REQUEST, 'items_per_page')) {
-        $params['items_per_page'] = $items_per_page;
-    }
-    if ($sort_by = fn_change_session_param(Tygh::$app['session']['search_params'], $_REQUEST, 'sort_by')) {
-        $params['sort_by'] = $sort_by;
-    }
-    if ($sort_order = fn_change_session_param(Tygh::$app['session']['search_params'], $_REQUEST, 'sort_order')) {
-        $params['sort_order'] = $sort_order;
+    foreach ($department_data['workers_ids'] as $worker => $id) {
+        $department_data['workers_ids'][$worker] = fn_get_user_short_info($id);
+        $worker++;
     }
 
-    list($products, $search) = fn_get_products($params, Registry::get('settings.Appearance.products_per_page'));
-
-
-    fn_filters_handle_search_result($params, $products, $search);
-
-    fn_gather_additional_products_data($products, [
-        'get_icon'      => true,
-        'get_detailed'  => true,
-        'get_options'   => true,
-        'get_discounts' => true,
-        'get_features'  => false
-    ]);
-
-    $selected_layout = fn_get_products_layout($_REQUEST);
-    Tygh::$app['view']->assign('products', $products);
-    Tygh::$app['view']->assign('search', $search);
-    Tygh::$app['view']->assign('selected_layout', $selected_layout);
+    Tygh::$app['view']->assign('workers', $department_data['workers_ids']);
+//    Tygh::$app['view']->assign('search', $search);
+//    Tygh::$app['view']->assign('selected_layout', $selected_layout);
 }
--- design/themes/responsive/templates/views/products/collection.tpl
+++ design/themes/responsive/templates/views/products/collection.tpl
@@ -17,12 +17,14 @@
     </div>
 
     {if $products}
+
         {assign var="layouts" value=""|fn_get_products_views:false:0}
         {if $layouts.$selected_layout.template}
             {include file="`$layouts.$selected_layout.template`" columns=$settings.Appearance.columns_in_products_list}
         {/if}
+
     {else}
-        <p class="ty-no-items">{__("text_no_products")}</p>
+        <p class="ty-no-items">{__("text_no_workers")}</p>
     {/if}
     <!--product_features_{$block.block_id}--></div>
 {capture name="mainbox_title"}{$collection_data.collection nofilter}{/capture}
